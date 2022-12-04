a6e203d920ceb529f2781dbb7493c054c79b99b9
BOOMERBOB69 <93742983+BOOMERBOB69@users.noreply.github.com>
2022-12-04 13:45:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,118 +1,100 @@
-let balance = 0.01;
-let titleAnimation = ["C", "O", "O", "K", "I", "E", " ", "C", "L", "I", "C", "K", "E", "R"]
-let title = ""
-let startingIndex = -1;
-let time = 3;
-let timer = setInterval(animateTitleTimer, 300);
-let restart = false;
-
-setTimeout(restartAnimation, 7200);;
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-function restartAnimation() {
-	time = 3;
-	timer = setInterval(animateTitleTimer, 300);
-	restart = false;
-}
-
-function animateTitleTimer(){
-	time--;
-	if (!restart) {
-		restart = true;
-		animateTitle();
-	}
-	if(time === 0 && startingIndex != 13) {
-		time = 1;
-		animateTitle();
-	}
-	else {
-		startingIndex = -1;
-		title = "";
-	}
-}
-
-function animateTitle() {
-	/*
-	if (startingIndex++ >= 13) {
-		startingIndex = -1;
-		title = ""
-	}
-	else {
-	*/
-		startingIndex += 1;
-		title += titleAnimation[startingIndex];
-		document.getElementById("title").innerHTML = title;
-	//}
-}
-
-function clickedMain() {
-	balance += 1;
-	restartAnimation();
-	clicked();
-}
-
+let clicks = 0
 
 function clicked() {
-	document.getElementById("balance").innerHTML = "Balance: $" + balance;
+	clicks++;
+	document.getElementById("clicks").innerHTML = "Clicked: " + clicks;
 }
 
-var $el = $("#very-specific-design");
-var elHeight = $el.outerHeight();
-var elWidth = $el.outerWidth();
-
-var $wrapper = $("#scaleable-wrapper");
-
-$wrapper.resizable({
-  resize: doResize
-});
-
-function doResize(event, ui) {
-  
-  var scale, origin;
-    
-  scale = Math.min(
-    ui.size.width / elWidth,    
-    ui.size.height / elHeight
-  );
-  
-  $el.css({
-    transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
-  });
-  
-}
-
-var starterData = { 
-  size: {
-    width: $wrapper.width(),
-    height: $wrapper.height()
-  }
-}
-
-doResize(null, starterData);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+49 106 104 102 32 118 106 32 103 104 32 106 118 103 113 117 32 103 105 32 114 113 118 103 105 105 112 106 52 56 57 64 111 118 103 113 117 46 108 119 118 32 105 119 32 108 117 103 113 118 32 54 119 50 122 32 120 122 113 55 106 46 32 116 104 101 32 107 101 121 32 105 115 32 109 121 32 102 97 118 111 117 114 105 116 101 32 99 104 105 108 100 46
+
+*/
